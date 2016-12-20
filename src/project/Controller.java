fd4abcaff3357bb4869d1db945942b05f403f7ca
THINKPAD-MAX\makss <makss56@gmail.com>
2016-12-21 01:12:30 +0300

    JavaFX app added

--- src/project/Controller.java
+++ src/project/Controller.java
@@ -1,4 +1,5 @@
-package sample;
+package project;
+
 
 public class Controller {
 }
--- src/project/Main.java
+++ src/project/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/gui.fxml"));
+        primaryStage.setTitle("Computer Graphics Application");
+        primaryStage.setScene(new Scene(root, 600, 300));
         primaryStage.show();
     }
 
